230472c80c06a46252be028facb4593101121680
Ryan Youngdale <youngdale95@gmail.com>
2018-03-03 19:41:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,8 +24,8 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  
   array.pop();
+  return array;
 }
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length-1);
