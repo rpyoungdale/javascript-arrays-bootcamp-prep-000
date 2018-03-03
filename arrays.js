71c4dc96bf11377ef76bd5a2adcbe789c7e4150a
Ryan Youngdale <youngdale95@gmail.com>
2018-03-03 19:31:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,6 +25,6 @@ function removeElementFromBeginningOfArray(array) {
 function destructivelyRemoveElementFromEndOfArray(array) {
   return array.pop();
 }
-/*function removeElementFromEndOfArray(array) {
+function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length-1);
-}*/
+}
