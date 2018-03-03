518293ed500a596fc5049f5687a001006d0277de
Ryan Youngdale <youngdale95@gmail.com>
2018-03-03 19:27:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,11 +2,11 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat",  "skittles"];
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
-/*function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
-function addElementToEndOfArray(array, element) {
+/*function addElementToEndOfArray(array, element) {
   var newArray2 = array.push(element);
   return newArray2;
 }
