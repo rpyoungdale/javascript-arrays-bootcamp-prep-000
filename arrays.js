075f59c535d285cb47b671dddfa7e05483271f15
Ryan Youngdale <youngdale95@gmail.com>
2018-03-03 19:25:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat",  "skittles"];
 function addElementToBeginningOfArray(array, element) {
-  return array = [element, ...]
+  return [element, ...array];
 }
 /*function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
